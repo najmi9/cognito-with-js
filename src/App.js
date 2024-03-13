acad40f0364e2ebe3e6c4a1140fa8ed3b6592555
Imad Najmi <imadnajmi9@gmail.com>
2024-03-13 11:44:56 +0100

    use aws with sdk

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {
+  CognitoUserPool,
+  CognitoUser,
+  AuthenticationDetails
+} from 'amazon-cognito-identity-js';
+
 function App() {
+  const poolData = {
+    UserPoolId: '',
+    ClientId: '',
+  };
+
+  const username = '';
+  const password = ''; // Changed Password to password (camelCase)
+
+  const userPool = new CognitoUserPool(poolData);
+  const cognitoUser = new CognitoUser({
+    Username: username,
+    Pool: userPool,
+  });
+
+  const authData = {
+    Username: username,
+    Password: password,
+  };
+
+  const authenticationDetails = new AuthenticationDetails(authData);
+
+  const authenticateUser = () => {
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: (data) => {
+        const totpMfaSettings = {
+          PreferredMfa: true,
+          Enabled: true,
+        };
+        cognitoUser.setUserMfaPreference(null, totpMfaSettings, function (err, result) {
+          if (err) {
+            alert(err.message || JSON.stringify(err));
+          }
+          console.log('call result ' + result);
+        });
+      },
+      onFailure: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={authenticateUser}>Enable MFA</button>
       </header>
     </div>
   );
